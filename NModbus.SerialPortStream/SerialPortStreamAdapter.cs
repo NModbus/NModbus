0c4bc8be6d272d0cfcc1dde8c877404c55c8a3f5
Rich. Quackenbush <rich.quackenbush@captiveaire.com>
2017-11-17 14:32:49 -0500

    In progress work on a SerialPortStream adapter.

---
+++ NModbus.SerialPortStream/SerialPortStreamAdapter.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using RJCP.IO.Ports;
+using NModbus.IO;
+
+// ReSharper disable once CheckNamespace
+namespace NModbus.Serial
+{
+    /// <summary>
+    /// An adapter for the SerialPortStream class. Useful for getting serial port access on non-Windows devices.
+    /// </summary>
+    public class SerialPortStreamAdapter : IStreamResource
+    {
+        private readonly SerialPortStream _serialPortStream;
+
+        public SerialPortStreamAdapter(SerialPortStream serialPortStream)
+        {
+            _serialPortStream = serialPortStream;
+        }
+
+        public int InfiniteTimeout => SerialPortStream.InfiniteTimeout;
+
+        public int ReadTimeout
+        {
+            get => _serialPortStream.ReadTimeout;
+            set => _serialPortStream.ReadTimeout = value;
+        }
+        public int WriteTimeout
+        {
+            get => _serialPortStream.WriteTimeout;
+            set => _serialPortStream.WriteTimeout = value;
+        }
+
+        public void DiscardInBuffer()
+        {
+            _serialPortStream.Flush();
+        }
+
+        public int Read(byte[] buffer, int offset, int count)
+        {
+            int result = _serialPortStream.Read(buffer, offset, count);
+
+            if (result == 0)
+                throw new TimeoutException();
+
+            return result;
+        }
+
+        public void Write(byte[] buffer, int offset, int count)
+        {
+            _serialPortStream.Write(buffer, offset, count);
+        }
+
+        public void Dispose()
+        {
+            _serialPortStream.Dispose();
+        }
+    }
+}
