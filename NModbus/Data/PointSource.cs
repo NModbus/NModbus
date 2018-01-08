bec1a51346df0f9a72acda68a4cd02e718922db2
Lubomír Laski <lubomir.laski@sde.cz>
2018-01-08 19:19:43 +0100

    ISlaveDataStore default implementation

---
+++ NModbus/Data/PointSource.cs
@@ -0,0 +1,64 @@
+﻿using NModbus.Device;
+using NModbus.Unme.Common;
+using System;
+using System.Linq;
+
+namespace NModbus.Data
+{
+    /// <summary>
+    /// A simple implementation of the point source. All registers are.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class PointSource<T> : IPointSource<T> where T : struct
+    {
+        public event EventHandler<PointEventArgs> BeforeRead;
+
+        public event EventHandler<PointEventArgs<T>> BeforeWrite;
+
+        public event EventHandler<PointEventArgs> AfterWrite;
+
+        //Only create this if referenced.
+        private readonly Lazy<T[]> _points;
+
+        private readonly object _syncRoot = new object();
+
+        public PointSource()
+        {
+            _points = new Lazy<T[]>(() => new T[ushort.MaxValue]);
+        }
+
+        public T[] ReadPoints(ushort startAddress, ushort numberOfPoints)
+        {
+            lock (_syncRoot)
+            {
+                return _points.Value
+                    .Slice(startAddress, numberOfPoints)
+                    .ToArray();
+            }
+        }
+
+        T[] IPointSource<T>.ReadPoints(ushort startAddress, ushort numberOfPoints)
+        {
+            BeforeRead?.Invoke(this, new PointEventArgs(startAddress, numberOfPoints));
+            return ReadPoints(startAddress, numberOfPoints);
+        }
+
+        public void WritePoints(ushort startAddress, T[] points)
+        {
+            lock (_syncRoot)
+            {
+                for (ushort index = 0; index < points.Length; index++)
+                {
+                    _points.Value[startAddress + index] = points[index];
+                }
+            }
+        }
+
+        void IPointSource<T>.WritePoints(ushort startAddress, T[] points)
+        {
+            BeforeWrite?.Invoke(this, new PointEventArgs<T>(startAddress, points));
+            WritePoints(startAddress, points);
+            AfterWrite?.Invoke(this, new PointEventArgs(startAddress, (ushort)points.Length));
+        }
+    }
+}
---
+++ NModbus/Device/PointEventArgs.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+
+namespace NModbus.Device
+{
+    /// <summary>
+    /// Modbus Slave request event args containing information on the message.
+    /// </summary>
+    public class PointEventArgs : EventArgs
+    {
+        private ushort _numberOfPoints;
+
+        private ushort _startAddress;
+
+        public PointEventArgs(ushort startAddress, ushort numberOfPoints)
+        {
+            _startAddress = startAddress;
+            _numberOfPoints = numberOfPoints;
+        }
+
+        public ushort NumberOfPoints => _numberOfPoints;
+
+        public ushort StartAddress => _startAddress;
+    }
+}
