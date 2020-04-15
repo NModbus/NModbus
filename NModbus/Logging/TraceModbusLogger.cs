7291d4d39979578a28de6c54186eca59ce4b0904
Rich Quackenbush <rich.quackenbush@gmail.com>
2020-04-15 10:00:33 -0400

    Added TraceModbusLogger
    https://github.com/NModbus/NModbus/issues/60

---
+++ NModbus/Logging/TraceModbusLogger.cs
@@ -0,0 +1,28 @@
+﻿#if NETSTANDARD1_3
+#else
+
+using System;
+using System.Diagnostics;
+
+namespace NModbus.Logging
+{
+    public class TraceModbusLogger : ModbusLogger
+    {
+        private const int LevelColumnSize = 15;
+        private static readonly string BlankHeader = Environment.NewLine + new string(' ', LevelColumnSize);
+
+        public TraceModbusLogger(LoggingLevel minimumLoggingLevel = LoggingLevel.Debug)
+            : base(minimumLoggingLevel)
+        {
+        }
+
+        protected override void LogCore(LoggingLevel level, string message)
+        {
+            message = message?.Replace(Environment.NewLine, BlankHeader);
+
+            Trace.WriteLine($"[{level}]".PadRight(LevelColumnSize) + message);
+        }
+    }
+}
+
+#endif
