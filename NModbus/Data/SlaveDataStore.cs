a2e64067a93baa89155a4a98468cade0b0bd0881
Rich Quackenbush <rich.quackenbush@gmail.com>
2020-04-15 09:45:35 -0400

    Implement ISlaveDataStore implicitly
    https://github.com/NModbus/NModbus/issues/59

--- NModbus/Data/SlaveDataStore.cs
+++ NModbus/Data/SlaveDataStore.cs
@@ -2,17 +2,12 @@
 {
     public class SlaveDataStore : ISlaveDataStore
     {
-        private readonly IPointSource<ushort> _holdingRegisters = new PointSource<ushort>();
-        private readonly IPointSource<ushort> _inputRegisters = new PointSource<ushort>();
-        private readonly IPointSource<bool> _coilDiscretes = new PointSource<bool>();
-        private readonly IPointSource<bool> _coilInputs = new PointSource<bool>();
+        public IPointSource<ushort> HoldingRegisters { get; } = new PointSource<ushort>();
 
-        IPointSource<ushort> ISlaveDataStore.HoldingRegisters => _holdingRegisters;
+        public IPointSource<ushort> InputRegisters { get; } = new PointSource<ushort>();
 
-        IPointSource<ushort> ISlaveDataStore.InputRegisters => _inputRegisters;
+        public IPointSource<bool> CoilDiscretes { get; } = new PointSource<bool>();
 
-        IPointSource<bool> ISlaveDataStore.CoilDiscretes => _coilDiscretes;
-
-        IPointSource<bool> ISlaveDataStore.CoilInputs => _coilInputs;
+        public IPointSource<bool> CoilInputs { get; } = new PointSource<bool>();
     }
 }
